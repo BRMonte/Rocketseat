6581929d77169ae58c517e9ad7e5f5c182cf09e8
Bruno Montebello <bruno.montebello@nexoos.com.br>
2020-08-26 12:38:32 +0100

    Store html elements in JS variables

---
+++ ToDoList/index.js
@@ -0,0 +1,3 @@
+var listElement = document.querySelector('#app ul'); //armazeno na variavel o elemento UL que da dentro da div #app
+var inputElement = document.querySelector('#app input'); 
+var buttonElement = document.querySelector('#app button'); 
